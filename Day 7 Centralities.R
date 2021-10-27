ed23a53618d01301e038fc7a97fc429e6d1b9941
Dan <danielfaltesek@gmail.com>
2021-10-26 17:52:33 -0700

    Updated Roads Data and centralities

---
+++ Day 7 Centralities.R
@@ -0,0 +1,66 @@
+#library list
+library(sna)
+library(network)
+library(ggplot2)
+library(dplyr)
+library(ggnetwork)
+
+#The Road...
+#produce a network
+R<-network(roads, directed = FALSE, matrix.type = "edgelist")
+#node attributes
+set.vertex.attribute(R, "between", betweenness(R))
+set.vertex.attribute(R, "close", closeness(R))
+set.vertex.attribute(R, "count", degree(R))
+
+#about that edge
+set.edge.attribute(R, "where", road$WHERE)
+
+#WARNING - this code takes a long time to render
+#target layout - arranged by degree, labels offset 5 from dots, outliers way out side
+L<-ggnetwork(R, layout = "target", "between", circ.lab.offset=5, periph.outside=TRUE)
+
+#eigenvector based layourt using the strong assumptions, you can switch to weak if you want
+K<-ggnetwork(R, layout = "eigen", "symstrong")
+
+#area is size, cool controls Simulated Annealing - bigger numbers bigger crystals 
+P<-ggnetwork(R, layout = "fruchtermanreingold", area = 100, cool=2)
+
+#a physical simulation of a spring process - kilograms per newton meter 
+#mass is in KG - heavy like bowling balls
+#equi is when springs and mass balance
+#repe
+#kfr is friction - I have it turned down....
+#repulsive forces are turned on...
+P<-ggnetwork(R, layout = "spring", mass=5, equil=.1, repeqdis=1, kfr = .001, repulse=TRUE)
+
+#highway plots
+ggplot(P, aes(x, y, xend = xend, yend = yend)) +
+  geom_edges(aes(colour=where)) +
+  geom_nodes(aes(size = between, alpha = 1/count)) +
+  geom_nodetext_repel(aes(label = vertex.names))+
+  theme_blank() +
+  theme(legend.position = "bottom")
+
+#overall centralization
+centralization(R, betweenness)
+
+#lets make a table
+highway_data<-data.frame(
+  road=get.vertex.attribute(R, "vertex.names"),
+  degree=degree(R, cmode="indegree"),
+  between=betweenness(R),
+  close=closeness(R),
+  #eigenvector prestige, degree to which nominated
+  prestige=prestige(R, cmode = "eigenvector", rescale=TRUE),
+  #prestige by proximity weighted domain prestige
+  prestige2=prestige(R, cmode = "domain.proximity", rescale=TRUE),
+  #measure like bonacich power centrality
+  information=infocent(R), 
+  bonpow=bonpow(R),
+  eigenvector=evcent(R),
+  load=loadcent(R)
+)
+
+cutpoints(R)
+maxflow(R)
